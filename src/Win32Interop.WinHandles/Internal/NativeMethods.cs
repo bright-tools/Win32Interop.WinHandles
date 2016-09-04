2135d3592686bd7fe1c612c77d607db95bc9cf13
bright-tools <dev@brightsilence.com>
2016-09-04 18:13:47 +0100

    Add GetWindowExec() to retrieve exe file associated with window

--- src/Win32Interop.WinHandles/Internal/NativeMethods.cs
+++ src/Win32Interop.WinHandles/Internal/NativeMethods.cs
@@ -15,6 +15,21 @@ namespace Win32Interop.WinHandles.Internal
     public const bool EnumWindows_ContinueEnumerating = true;
     public const bool EnumWindows_StopEnumerating = false;
 
+    [Flags]
+    public enum ProcessAccessFlags : uint
+    {
+        All = 0x001F0FFF,
+        Terminate = 0x00000001,
+        CreateThread = 0x00000002,
+        VMOperation = 0x00000008,
+        VMRead = 0x00000010,
+        VMWrite = 0x00000020,
+        DupHandle = 0x00000040,
+        SetInformation = 0x00000200,
+        QueryInformation = 0x00000400,
+        Synchronize = 0x00100000
+    }
+
     [DllImport("user32.dll")]
     public static extern bool EnumWindows(EnumWindowsProc enumProc, IntPtr lParam);
 
@@ -29,6 +44,12 @@ namespace Win32Interop.WinHandles.Internal
 
     [DllImport("user32.dll", SetLastError = true)]
     internal static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint processId);
+
+    [DllImport("kernel32.dll")]
+    internal static extern IntPtr OpenProcess(ProcessAccessFlags dwDesiredAccess, [MarshalAs(UnmanagedType.Bool)] bool bInheritHandle, uint dwProcessId);
+
+    [DllImport("psapi.dll")]
+    internal static extern uint GetModuleFileNameEx(IntPtr hProcess, IntPtr hModule, [Out] StringBuilder lpBaseName, [In] [MarshalAs(UnmanagedType.U4)] int nSize);
 
     [DllImport("user32.dll", CharSet = CharSet.Unicode)]
     internal static extern int GetWindowText(IntPtr hWnd, StringBuilder strText, int maxCount);
--- src/Win32Interop.WinHandles/WindowHandleExtensions.cs
+++ src/Win32Interop.WinHandles/WindowHandleExtensions.cs
@@ -7,50 +7,64 @@ using Win32Interop.WinHandles.Internal;
 
 namespace Win32Interop.WinHandles
 {
-  /// <summary> Extension methods for <see cref="WindowHandle"/> </summary>
-  public static class WindowHandleExtensions
-  {
-    /// <summary> Check if the given window handle is currently visible. </summary>
-    /// <param name="windowHandle"> The window to act on. </param>
-    /// <returns> true if the window is visible, false if not. </returns>
-    public static bool IsVisible(this WindowHandle windowHandle)
+    /// <summary> Extension methods for <see cref="WindowHandle"/> </summary>
+    public static class WindowHandleExtensions
     {
-      return NativeMethods.IsWindowVisible(windowHandle.RawPtr);
+        /// <summary> Check if the given window handle is currently visible. </summary>
+        /// <param name="windowHandle"> The window to act on. </param>
+        /// <returns> true if the window is visible, false if not. </returns>
+        public static bool IsVisible(this WindowHandle windowHandle)
+        {
+            return NativeMethods.IsWindowVisible(windowHandle.RawPtr);
+        }
+
+        /// <summary> Gets the Win32 class name of the given window. </summary>
+        /// <param name="windowHandle"> The window handle to act on. </param>
+        /// <returns> The class name of the passed in window. </returns>
+        public static string GetClassName(this WindowHandle windowHandle)
+        {
+            int size = 255;
+            int actualSize = 0;
+            StringBuilder builder;
+            do
+            {
+                builder = new StringBuilder(size);
+                actualSize = NativeMethods.GetClassName(windowHandle.RawPtr, builder, builder.Capacity);
+                size *= 2;
+            } while (actualSize == size - 1);
+
+            return builder.ToString();
+        }
+
+        /// <summary> Gets the text associated with the given window handle. </summary>
+        /// <param name="windowHandle"> The window handle to act on. </param>
+        /// <returns> The window text. </returns>
+        [NotNull]
+        public static string GetWindowText(this WindowHandle windowHandle)
+        {
+            int size = NativeMethods.GetWindowTextLength(windowHandle.RawPtr);
+            if (size > 0)
+            {
+                var builder = new StringBuilder(size + 1);
+                NativeMethods.GetWindowText(windowHandle.RawPtr, builder, builder.Capacity);
+                return builder.ToString();
+            }
+
+            return String.Empty;
+        }
+
+        /// <summary> Gets the executable associated with the given window handle. </summary>
+        /// <param name="windowHandle"> The window handle to act on. </param>
+        /// <returns> The executable filename. </returns>
+        [NotNull]
+        public static string GetWindowExec(this WindowHandle windowHandle)
+        {
+            uint pid;
+            NativeMethods.GetWindowThreadProcessId(windowHandle.RawPtr, out pid);
+            IntPtr proc = NativeMethods.OpenProcess(NativeMethods.ProcessAccessFlags.VMRead | NativeMethods.ProcessAccessFlags.QueryInformation, true, pid);
+            var buff = new StringBuilder(1024);
+            NativeMethods.GetModuleFileNameEx(proc, (IntPtr)0, buff, buff.Capacity);
+            return buff.ToString();
+        }
     }
-
-    /// <summary> Gets the Win32 class name of the given window. </summary>
-    /// <param name="windowHandle"> The window handle to act on. </param>
-    /// <returns> The class name of the passed in window. </returns>
-    public static string GetClassName(this WindowHandle windowHandle)
-    {
-      int size = 255;
-      int actualSize = 0;
-      StringBuilder builder;
-      do
-      {
-        builder = new StringBuilder(size);
-        actualSize = NativeMethods.GetClassName(windowHandle.RawPtr, builder, builder.Capacity);
-        size *= 2;
-      } while (actualSize == size - 1);
-
-      return builder.ToString();
-    }
-
-    /// <summary> Gets the text associated with the given window handle. </summary>
-    /// <param name="windowHandle"> The window handle to act on. </param>
-    /// <returns> The window text. </returns>
-    [NotNull]
-    public static string GetWindowText(this WindowHandle windowHandle)
-    {
-      int size = NativeMethods.GetWindowTextLength(windowHandle.RawPtr);
-      if (size > 0)
-      {
-        var builder = new StringBuilder(size + 1);
-        NativeMethods.GetWindowText(windowHandle.RawPtr, builder, builder.Capacity);
-        return builder.ToString();
-      }
-
-      return String.Empty;
-    }
-  }
 }
