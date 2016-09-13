5296849f49618c76ab850418e3a1fecb617aeb96
bright-tools <dev@brightsilence.com>
2016-09-13 21:48:39 +0100

    Expose additional native methods

--- src/Win32Interop.WinHandles/Internal/NativeMethods.cs
+++ src/Win32Interop.WinHandles/Internal/NativeMethods.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
-using System.Threading.Tasks;
+using static Win32Interop.WinHandles.WindowHandleExtensions;
 
 namespace Win32Interop.WinHandles.Internal
 {
@@ -28,7 +26,15 @@ namespace Win32Interop.WinHandles.Internal
         SetInformation = 0x00000200,
         QueryInformation = 0x00000400,
         Synchronize = 0x00100000
-    }
+    };
+
+    [Flags]
+    public enum WindowPosFlags : uint
+    {
+        NOZORDER = 0x4,
+        NOMOVE = 0x2,
+        NOSIZE = 0x1
+    };
 
     [DllImport("user32.dll")]
     public static extern bool EnumWindows(EnumWindowsProc enumProc, IntPtr lParam);
@@ -57,9 +63,38 @@ namespace Win32Interop.WinHandles.Internal
     [DllImport("user32.dll", CharSet = CharSet.Unicode)]
     internal static extern int GetWindowTextLength(IntPtr hWnd);
 
+    [DllImport("user32.dll", EntryPoint = "SetWindowPos")]
+    public static extern IntPtr SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int Y, int cx, int cy, WindowPosFlags wFlags);
+
+    /// <summary>
+    /// Retrieves the show state and the restored, minimized, and maximized positions of the specified window.
+    /// </summary>
+    /// <param name="hWnd">
+    /// A handle to the window.
+    /// </param>
+    /// <param name="lpwndpl">
+    /// A pointer to the WINDOWPLACEMENT structure that receives the show state and position information.
+    /// <para>
+    /// Before calling GetWindowPlacement, set the length member to sizeof(WINDOWPLACEMENT). GetWindowPlacement fails if lpwndpl-> length is not set correctly.
+    /// </para>
+    /// </param>
+    /// <returns>
+    /// If the function succeeds, the return value is nonzero.
+    /// <para>
+    /// If the function fails, the return value is zero. To get extended error information, call GetLastError.
+    /// </para>
+    /// </returns>
+    [DllImport("user32.dll", SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static extern bool GetWindowPlacement(IntPtr hWnd, ref WINDOWPLACEMENT lpwndpl);
+
     [DllImport("user32.dll")]
     internal static extern int GetClassName(IntPtr hWnd,
                                             StringBuilder lpClassName,
                                             int nMaxCount);
-  }
+
+    [DllImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    internal static extern bool ShowWindow(IntPtr hwnd, WindowShowStyle nCmdShow);
+    }
 }
--- src/Win32Interop.WinHandles/WindowHandleExtensions.cs
+++ src/Win32Interop.WinHandles/WindowHandleExtensions.cs
@@ -4,12 +4,115 @@ using System.Linq;
 using System.Text;
 using JetBrains.Annotations;
 using Win32Interop.WinHandles.Internal;
+using System.Runtime.InteropServices;
 
 namespace Win32Interop.WinHandles
 {
     /// <summary> Extension methods for <see cref="WindowHandle"/> </summary>
     public static class WindowHandleExtensions
     {
+        /// <summary>Enumeration of the different ways of showing a window using 
+        /// ShowWindow</summary>
+        public enum WindowShowStyle : uint
+        {
+            /// <summary>Hides the window and activates another window.</summary>
+            /// <remarks>See SW_HIDE</remarks>
+            Hide = 0,
+            /// <summary>Activates and displays a window. If the window is minimized 
+            /// or maximized, the system restores it to its original size and 
+            /// position. An application should specify this flag when displaying 
+            /// the window for the first time.</summary>
+            /// <remarks>See SW_SHOWNORMAL</remarks>
+            ShowNormal = 1,
+            /// <summary>Activates the window and displays it as a minimized window.</summary>
+            /// <remarks>See SW_SHOWMINIMIZED</remarks>
+            ShowMinimized = 2,
+            /// <summary>Activates the window and displays it as a maximized window.</summary>
+            /// <remarks>See SW_SHOWMAXIMIZED</remarks>
+            ShowMaximized = 3,
+            /// <summary>Maximizes the specified window.</summary>
+            /// <remarks>See SW_MAXIMIZE</remarks>
+            Maximize = 3,
+            /// <summary>Displays a window in its most recent size and position. 
+            /// This value is similar to "ShowNormal", except the window is not 
+            /// actived.</summary>
+            /// <remarks>See SW_SHOWNOACTIVATE</remarks>
+            ShowNormalNoActivate = 4,
+            /// <summary>Activates the window and displays it in its current size 
+            /// and position.</summary>
+            /// <remarks>See SW_SHOW</remarks>
+            Show = 5,
+            /// <summary>Minimizes the specified window and activates the next 
+            /// top-level window in the Z order.</summary>
+            /// <remarks>See SW_MINIMIZE</remarks>
+            Minimize = 6,
+            /// <summary>Displays the window as a minimized window. This value is 
+            /// similar to "ShowMinimized", except the window is not activated.</summary>
+            /// <remarks>See SW_SHOWMINNOACTIVE</remarks>
+            ShowMinNoActivate = 7,
+            /// <summary>Displays the window in its current size and position. This 
+            /// value is similar to "Show", except the window is not activated.</summary>
+            /// <remarks>See SW_SHOWNA</remarks>
+            ShowNoActivate = 8,
+            /// <summary>Activates and displays the window. If the window is 
+            /// minimized or maximized, the system restores it to its original size 
+            /// and position. An application should specify this flag when restoring 
+            /// a minimized window.</summary>
+            /// <remarks>See SW_RESTORE</remarks>
+            Restore = 9,
+            /// <summary>Sets the show state based on the SW_ value specified in the 
+            /// STARTUPINFO structure passed to the CreateProcess function by the 
+            /// program that started the application.</summary>
+            /// <remarks>See SW_SHOWDEFAULT</remarks>
+            ShowDefault = 10,
+            /// <summary>Windows 2000/XP: Minimizes a window, even if the thread 
+            /// that owns the window is hung. This flag should only be used when 
+            /// minimizing windows from a different thread.</summary>
+            /// <remarks>See SW_FORCEMINIMIZE</remarks>
+            ForceMinimized = 11
+        }
+
+        [StructLayout(LayoutKind.Explicit, Size = 8, Pack = 4)]
+        public struct Point
+        {
+            [FieldOffset(0)]
+            public int x;
+            [FieldOffset(4)]
+            public int y;
+        };
+
+        [StructLayout(LayoutKind.Explicit, Size = 16, Pack = 4)]
+        public struct Rect
+        {
+            [FieldOffset(0)]
+            public int left;
+            [FieldOffset(4)]
+            public int top;
+            [FieldOffset(8)]
+            public int right;
+            [FieldOffset(12)]
+            public int bottom;
+        }
+
+        [StructLayout(LayoutKind.Explicit, Size = 44, Pack = 4)]
+        public struct WINDOWPLACEMENT
+        {
+            [FieldOffset(0)]
+            public uint length;
+            [FieldOffset(4)]
+            public uint flags;
+            [FieldOffset(8)]
+            public WindowShowStyle showCmd;
+            [FieldOffset(12)]
+            public Point ptMinPosition;
+            [FieldOffset(20)]
+            public Point ptMaxPosition;
+            [FieldOffset(28)]
+            public Rect rcNormalPosition;
+        };
+
+
+
         /// <summary> Check if the given window handle is currently visible. </summary>
         /// <param name="windowHandle"> The window to act on. </param>
         /// <returns> true if the window is visible, false if not. </returns>
@@ -65,6 +168,37 @@ namespace Win32Interop.WinHandles
             var buff = new StringBuilder(1024);
             NativeMethods.GetModuleFileNameEx(proc, (IntPtr)0, buff, buff.Capacity);
             return buff.ToString();
+        }
+
+        public static void SetWindowXY(this WindowHandle windowHandle, int x, int y)
+        {
+            NativeMethods.SetWindowPos(windowHandle.RawPtr, 0, x, y, 100, 100, NativeMethods.WindowPosFlags.NOZORDER | NativeMethods.WindowPosFlags.NOSIZE );
+        }
+
+        public static void SetWindowSize(this WindowHandle windowHandle, int w, int h)
+        {
+            NativeMethods.SetWindowPos(windowHandle.RawPtr, 0, 0, 0, w, h, NativeMethods.WindowPosFlags.NOZORDER | NativeMethods.WindowPosFlags.NOMOVE );
+        }
+
+        public static bool GetWindowPlacement(this WindowHandle windowHandle, ref WINDOWPLACEMENT placement)
+        {
+            placement.length = (uint)(Marshal.SizeOf(placement));
+            return NativeMethods.GetWindowPlacement(windowHandle.RawPtr, ref placement);
+        }
+
+        public static bool MaximizeWindow(this WindowHandle windowHandle)
+        {
+            return NativeMethods.ShowWindow(windowHandle.RawPtr, WindowShowStyle.ShowMaximized);
+        }
+
+        public static bool MinimizeWindow(this WindowHandle windowHandle)
+        {
+            return NativeMethods.ShowWindow(windowHandle.RawPtr, WindowShowStyle.ShowMinimized);
+        }
+
+        public static bool ShowWindow(this WindowHandle windowHandle)
+        {
+            return NativeMethods.ShowWindow(windowHandle.RawPtr, WindowShowStyle.ShowNormal);
         }
     }
 }
